9ff89fa8f42e21196d76f2dd6646193365a27e98
Ramesh Fadatare <ramesh24fadatare@gmail.com>
2023-03-28 09:23:29 +0530

    Update WebMvcConfig.java

--- src/main/java/net/guides/springboot/springbootmultipledatasources/config/WebMvcConfig.java
+++ src/main/java/net/guides/springboot/springbootmultipledatasources/config/WebMvcConfig.java
@@ -7,14 +7,13 @@ import org.springframework.context.annotation.Bean;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * @author Ramesh Fadatare
  * 
  */
 @Configuration
-public class WebMvcConfig extends WebMvcConfigurerAdapter
+public class WebMvcConfig
 {
 	
 	@Bean
